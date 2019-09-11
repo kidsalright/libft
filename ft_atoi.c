23d7b5ec04e4feb65b025892226fda2472ffb170
Yahaira Berries <yberries@oa-i2.21-school.ru>
2019-09-11 14:39:04 +0300

    super atoi omg

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,32 +6,35 @@
 /*   By: yberries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 15:56:13 by yberries          #+#    #+#             */
-/*   Updated: 2019/09/09 21:28:30 by yberries         ###   ########.fr       */
+/*   Updated: 2019/09/11 14:33:49 by yberries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int		ft_atoi(const char *str)
 {
-	int i;
-	int number;
-	int negative;
+	int		i;
+	long	number;
+	char	negative;
+	char	*s;
 
+	s = ft_strdup(str);
 	i = 0;
 	number = 0;
 	negative = 0;
-	if (*str == '\f' || *str == '\n' || *str == '\r' || *str == '\v' || \
-				*str == ' ' || *str == '\t')
-		++i;
-	if (str[i] == '-')
-		negative = 1;
-	if (str[i] == '+' || str[i] == '-')
-		++i;
-	while ((str[i] >= '0') && (str[i] <= '9'))
+	while ((s[i] >= 9 && s[i] <= 13) || s[i] == 32)
+		i++;
+	if (s[i] == '-')
+		negative = -1;
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	while (ft_isdigit(s[i]))
 	{
-		number = 10 * number + (str[i] + '0');
-		++i;
+		number = 10 * number + (s[i] - '0');
+		i++;
 	}
-	if (negative == 1)
-		return (-number);
-	return (number);
+	if (negative == -1)
+		return ((int)number * negative);
+	return ((int)number);
 }
