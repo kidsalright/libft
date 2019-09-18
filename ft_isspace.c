77c08a256760ae8195c68fef7e410266a66a15ab
Yahaira Berries <yberries@oa-i2.21-school.ru>
2019-09-18 09:49:13 +0300

    some fixes))

--- ft_isspace.c
+++ ft_isspace.c
@@ -1,37 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_isspace.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yberries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/08 15:56:13 by yberries          #+#    #+#             */
-/*   Updated: 2019/09/18 09:41:18 by yberries         ###   ########.fr       */
+/*   Created: 2019/09/18 07:20:33 by yberries          #+#    #+#             */
+/*   Updated: 2019/09/18 07:22:20 by yberries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+int		ft_isspace(char c)
 {
-	int		sign;
-	size_t	n;
-
-	n = 0;
-	sign = 0;
-	while (*str == 32 || (*str >= 9 && *str <= 13))
-		++str;
-	if (*str == '-' || *str == '+')
-		sign = *str++ == '-';
-	while (*str >= '0' && *str <= '9')
-	{
-		n = n * 10 + (*str++ - '0');
-		if (n >= 9223372036854775807 && sign == 0)
-			return (-1);
-		else if (n > 9223372036854775807)
-			return (0);
-	}
-	if (sign)
-		return (-n);
-	return (n);
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	else
+		return (0);
 }
--- ft_root.c
+++ ft_root.c
@@ -1,37 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_root.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yberries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/08 15:56:13 by yberries          #+#    #+#             */
-/*   Updated: 2019/09/18 09:41:18 by yberries         ###   ########.fr       */
+/*   Created: 2019/09/18 09:15:54 by yberries          #+#    #+#             */
+/*   Updated: 2019/09/18 09:40:44 by yberries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+static int		ft_power(int nbr, int power)
 {
-	int		sign;
-	size_t	n;
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
+	return (nbr * ft_power(nbr, power - 1));
+}
 
-	n = 0;
-	sign = 0;
-	while (*str == 32 || (*str >= 9 && *str <= 13))
-		++str;
-	if (*str == '-' || *str == '+')
-		sign = *str++ == '-';
-	while (*str >= '0' && *str <= '9')
-	{
-		n = n * 10 + (*str++ - '0');
-		if (n >= 9223372036854775807 && sign == 0)
-			return (-1);
-		else if (n > 9223372036854775807)
-			return (0);
-	}
-	if (sign)
-		return (-n);
-	return (n);
+int				ft_root(int root, int nbr)
+{
+	int res;
+
+	res = 0;
+	while (ft_power(res, root) <= nbr)
+		++res;
+	return (res - 1);
 }
