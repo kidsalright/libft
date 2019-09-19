3f91ab77a843b1a54e1a5f31ef52c61eac243a0b
Yahaira Berries <yberries@oa-i2.21-school.ru>
2019-09-19 11:48:29 +0300

    atoi)

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,16 +6,17 @@
 /*   By: yberries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 15:56:13 by yberries          #+#    #+#             */
-/*   Updated: 2019/09/18 09:41:18 by yberries         ###   ########.fr       */
+/*   Updated: 2019/09/19 11:47:46 by yberries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 int		ft_atoi(const char *str)
 {
-	int		sign;
-	size_t	n;
+	char	sign;
+	long	n;
 
 	n = 0;
 	sign = 0;
@@ -26,12 +27,12 @@ int		ft_atoi(const char *str)
 	while (*str >= '0' && *str <= '9')
 	{
 		n = n * 10 + (*str++ - '0');
-		if (n >= 9223372036854775807 && sign == 0)
-			return (-1);
-		else if (n > 9223372036854775807)
-			return (0);
+		if (n >= LONG_MAX && sign == 0)
+			return ((int)LONG_MAX);
+		if ( n <= LONG_MIN)
+			return ((int)LONG_MIN);
 	}
 	if (sign)
-		return (-n);
-	return (n);
+		return ((int)-n);
+	return ((int)n);
 }
